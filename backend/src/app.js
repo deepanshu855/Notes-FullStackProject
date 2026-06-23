e8587b6ea567f700a476377bbd699b6c5f31bc4e
Deepanshu Sharma <deepanshusharma040@gmail.com>
2026-06-23 22:43:44 +0530

    update

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const path = require("path");
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(express.static("./public"));
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 connectToDb();
 
@@ -62,7 +62,7 @@ app.patch("/api/notes/:id", async (req, res) => {
 
 // Wild card route to integrate frontend
 app.use("*name", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "/public/index.html"));
+  res.sendFile(path.joinpath.join(__dirname, "..", "public", "index.html"));
 });
 
 module.exports = app;
