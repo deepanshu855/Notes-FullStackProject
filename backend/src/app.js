fef13125616176af30b6ffc5e3ea201354329182
Deepanshu Sharma <deepanshusharma040@gmail.com>
2026-06-23 20:42:17 +0530

    Implemented cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,14 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const connectToDb = require("./config/database.js");
 const noteModel = require("./model/notes.model.js");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 connectToDb();
 
 // POST-> /api/notes : to create notes.
